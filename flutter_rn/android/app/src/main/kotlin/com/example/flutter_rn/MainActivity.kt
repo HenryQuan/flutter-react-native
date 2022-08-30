c29b0f1005dd3604265c8b9f8bcb0fe780f30a60
Yiheng <development.henryquan@gmail.com>
2022-08-30 23:22:02 +1000

    running React Native in Flutter

--- flutter_rn/android/app/src/main/kotlin/com/example/flutter_rn/MainActivity.kt
+++ flutter_rn/android/app/src/main/kotlin/com/example/flutter_rn/MainActivity.kt
@@ -1,6 +1,34 @@
 package com.example.flutter_rn
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.Settings
+import io.flutter.BuildConfig
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    private val OVERLAY_PERMISSION_REQ_CODE = 1
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        /// https://reactnative.dev/docs/integration-with-existing-apps#3-configure-permissions-for-development-error-overlay
+        if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package: $packageName")
+                )
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+        }
+
+        val rn = Intent(this, ReactNativeActivity::class.java)
+        startActivity(rn)
+    }
 }
